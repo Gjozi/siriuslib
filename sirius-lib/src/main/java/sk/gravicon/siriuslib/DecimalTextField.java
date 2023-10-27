2080e5ffe54b11cde7b188a54e02a88f4369cbc7
gonczol <jgonczol@gmail.com>
2023-10-27 13:00:02 +0200

    2023.10.27 - doplnena desatinna ciarka ako oddelovac a monost neuvedenia oddelovaca desatinnych miest

--- sirius-lib/src/main/java/sk/gravicon/siriuslib/DecimalTextField.java
+++ sirius-lib/src/main/java/sk/gravicon/siriuslib/DecimalTextField.java
@@ -7,8 +7,8 @@ import javafx.scene.control.TextField;
 /**
  * JavaFX textové pole pre desatinné čísla. V fxml súbore aj scenebuildery je možné nastaviť
  * mask napr. ####.##
- * Ako desatinná čiarka sa používa bodka,
- * Ako číslo je možné použiť ľubovoľný znak okrem bodky 
+ * Ako desatinná čiarka sa používa bodka,alebo čiarka
+ * Ako číslo je možné použiť ľubovoľný znak okrem bodky a čiarky
  */
 public class DecimalTextField extends TextField {
 	private StringProperty mask;
@@ -22,10 +22,31 @@ public class DecimalTextField extends TextField {
 		mask = new SimpleStringProperty();
 		this.textProperty().addListener((observable, oldValue, newValue) -> {
 			String maskString = getMask();
-			int wholePart = maskString.indexOf(".");
+			int wholePart=-1;
+			wholePart = maskString.indexOf(".");
+			if(wholePart==-1) {
+				wholePart = maskString.indexOf(",");
+			}
+			//System.out.println("Nastavenie desatinnej ciarky"+wholePart);
+			int decDelimiter=-1;
+			decDelimiter = newValue.indexOf(".");
+			if (decDelimiter==-1) {
+				decDelimiter = newValue.indexOf(",");
+			}
+			//System.out.println("Pozicia desatinnej ciarky"+decDelimiter);
+			
 			int decimalPart = maskString.length() - wholePart-1;
-			String expr = "\\d{0,"+String.valueOf(wholePart)+"}([\\.\\,]\\d{0,"+String.valueOf(decimalPart)+"})?";
+			String expr;
+			if(wholePart>-1) {
+				expr = "\\d{0,"+String.valueOf(wholePart)+"}([\\.\\,]\\d{0,"+String.valueOf(decimalPart)+"})?";
+			} else {
+				expr = "\\d{0,"+String.valueOf(maskString.length())+"}";
+			}
+			
 			if (!newValue.matches(expr)) {
+				this.setText(oldValue);
+			}
+			if (wholePart==-1 && (decDelimiter>-1) ) {
 				this.setText(oldValue);
 			}
 		});
